de9281e2c7ac2766d12f29f7900ae6c0ddcf883f
@Your-Super-Shawn <shawnzhanghi@gmail.com>
2023-03-19 01:03:07 +1100

    Fix jest setup issue

---
+++ jest.setup.js
@@ -0,0 +1,13 @@
+// import '@testing-library/jest-dom/extend-expect'
+// import fetchMock from 'jest-fetch-mock'
+// import { TextEncoder, TextDecoder } from 'util';
+
+// global.TextEncoder = TextEncoder;
+// global.TextDecoder = TextDecoder;
+
+// fetchMock.enableMocks()
+
+import { TextEncoder, TextDecoder } from 'util';
+
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
