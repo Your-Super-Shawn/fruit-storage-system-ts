132a33bb921b7308df996c271b412eeab0a75462
@Your-Super-Shawn <shawnzhanghi@gmail.com>
2023-03-19 14:12:51 +1100

    Add more test cases

--- __test__/fruitStorage.test.ts
+++ __test__/fruitStorage.test.ts
@@ -5,35 +5,31 @@ import { FruitLimit } from "@/fruitStorage.domain/core/valueObjects/fruitLimit";
 import { FruitFactory } from "@/fruitStorage.domain/core/factories/fruitFactory";
 import { FruitRepository } from "@/fruitStorage.domain/core/repositories/fruitRepository";
 import { FruitService } from "@/fruitStorage.domain/services/fruitService";
+import { FruitAmount } from "@/fruitStorage.domain/core/valueObjects/fruitAmount";
 
-// Mock the FruitRepository
+// Mock the FruitRepository & FruitFactory
 jest.mock("../fruitStorage.domain/core/repositories/fruitRepository");
-
-// Mock the FruitFactory
 jest.mock("../fruitStorage.domain/core/factories/fruitFactory");
 
 const fruitRepository = new FruitRepository();
 const fruitFactory = new FruitFactory();
 const fruitService = new FruitService(fruitRepository, fruitFactory);
 
-const validFruit = {
+const lemon = {
   name: FruitName.create({ value: "lemon" }),
   description: FruitDescription.create({ value: "this is a lemon" }),
   limit: FruitLimit.create({ value: 5 }),
 };
 
 describe("Fruit storage system", () => {
-  beforeEach(() => {
+  beforeEach(async () => {
     // cleaning up the mess left behind the previous test
     jest.clearAllMocks();
   });
 
   // Task 1
   describe("createFruitForFruitStorage", () => {
-    /**
-     * Test 1.1 - Create a fruit called: lemon with the description this is a lemon and a limit of 10,
-     * this should pass & create a domain event.
-     */
+    // Test 1.1
     it("should create with lemon fruit.", async () => {
       // Arrange
       const fruit1 = new Fruit({
@@ -53,10 +49,7 @@ describe("Fruit storage system", () => {
       expect(createdFruit.description.value).toEqual("this is a lemon");
       expect(createdFruit.limit.value).toEqual(10);
     });
-    /**
-     * Test 1.2 - Create a fruit called: lemon with the description this is a fruit with a very long
-     * description and a limit of 10 ; verify this should fail
-     */
+    // Test 1.2
     it("should not create with very long description.", async () => {
       // Arrange
       const longDescription =
@@ -71,10 +64,7 @@ describe("Fruit storage system", () => {
         };
       }).toThrowError("Fruit description cannot be longer than 30 characters.");
     });
-    /**
-     * Test 1.3 - Create a fruit called: lemon with the description this is a lemon and a limit of 10
-     * twice, this should fail.
-     */
+    // Test 1.3
     it("should not create if the fruit exists", async () => {
       // Arrange
       const fruit3 = new Fruit({
@@ -115,14 +105,9 @@ describe("Fruit storage system", () => {
   // Task 2
   describe("updateFruitForFruitStorage", () => {
     beforeEach(async () => {
-      const lemon = new Fruit(validFruit);
       const createdLemon = await fruitFactory.createFruit(lemon);
     });
-    /**
-     * Test 2.1 - Assume the lemon fruit is already created in task 1, in the createFruitForStorage do
-     * this in a way you see fit (e.g. seeding the database in the test). Call the mutation to update
-     * the description of the lemon fruit to: updated lemon description , verify this should pass.
-     */
+    // Test 2.1
     it("should update description with appropriate length", async () => {
       // Arrange
       const updatedDescription = "updated lemon description";
@@ -131,25 +116,21 @@ describe("Fruit storage system", () => {
         .mockResolvedValueOnce();
       // Act
       const updatedLemon = await fruitRepository.updateFruitForFruitStorage(
-        validFruit.name,
+        lemon.name,
         FruitDescription.create({ value: updatedDescription }),
-        validFruit.limit
+        lemon.limit
       );
       // Assert
       expect(mockUpdateFruit).toHaveBeenCalledWith(
-        validFruit.name,
+        lemon.name,
         FruitDescription.create({ value: updatedDescription }),
-        validFruit.limit
+        lemon.limit
       );
       expect(mockUpdateFruit).toHaveBeenCalledTimes(1);
-      expect(updatedLemon).toBeNull;
+      expect(updatedLemon).toBeUndefined();
     });
-    /**
-     * Test 2.2 - Assume the lemon fruit is already created in test 1, call the createFruitForFruitStorage,
-     * do this in a way you see fit (e.g. seeding the database in the test). Call the updateFruitForFruitStorage
-     * mutation to update the description of the lemon fruit to: updated lemon with a long description , verify
-     * this should fail
-     */
+
+    // Test 2.2
     it("should not update with long description ", async () => {
       // Arrange
       const longDescription =
@@ -161,9 +142,9 @@ describe("Fruit storage system", () => {
           value: longDescription,
         });
         await fruitRepository.updateFruitForFruitStorage(
-          validFruit.name,
+          lemon.name,
           longDescriptionInstance,
-          validFruit.limit
+          lemon.limit
         );
       }).rejects.toThrowError(
         new Error("Fruit description cannot be longer than 30 characters.")
@@ -174,101 +155,146 @@ describe("Fruit storage system", () => {
   // Task 3
   describe("deleteFruitFromFruitStorage", () => {
     beforeEach(async () => {
-      const lemon = new Fruit(validFruit);
+      // setup initial valid fruit -lemon
       await fruitFactory.createFruit(lemon);
     });
-    /**
-     * Test 3.1 - Assume the lemon fruit is already created in task 1, in the createFruitForFruitStorage,
-     * do this in a way you see fit (e.g. seeding the database in the test) with an amount of 5 lemons.
-     * Call deleteFruitFromFruitStorage this should fail
-     */
+    // Test 3.1
     it("should not delete if the fruit have left amount and forceDelete is false", async () => {
-      // Arrange
-      const mockDeleteFruit = jest
-        .spyOn(fruitRepository, "deleteFruitFromFruitStorage")
-        .mockResolvedValueOnce();
-      // Act
-      await fruitRepository.deleteFruitFromFruitStorage(validFruit.name, false);
-      // Assert
-      expect(mockDeleteFruit).toHaveBeenCalledWith(validFruit.name, false);
-      expect(mockDeleteFruit).toHaveBeenCalledTimes(1);
+      // Act & Assert
+      try {
+        await fruitRepository.deleteFruitFromFruitStorage(lemon.name, false);
+      } catch (error: any) {
+        expect(error.message).toBe(
+          "Cannot delete fruit with a non-zero limit."
+        );
+      }
     });
-    /**
-     * Test 3.2 - Assume the lemon fruit is already created in task 1, in the createFruitForFruitStorage,
-     * do this in a way you see fit (e.g. seeding the database in the test) with an amount of 5 lemons.
-     * Call deleteFruitFromFruitStorage with forceDelete=true this should pass & create a domain event.
-     */
+    // Test 3.2
     it("should delete if forceDelete is true", async () => {
       // Arrange
       const mockDeleteFruit = jest
         .spyOn(fruitRepository, "deleteFruitFromFruitStorage")
         .mockResolvedValueOnce();
       // Act
-      await fruitRepository.deleteFruitFromFruitStorage(validFruit.name, true);
+      const deletedLemon = await fruitRepository.deleteFruitFromFruitStorage(
+        lemon.name,
+        true
+      );
       // Assert
-      expect(mockDeleteFruit).toHaveBeenCalledWith(validFruit.name, true);
+      expect(mockDeleteFruit).toHaveBeenCalledWith(lemon.name, true);
       expect(mockDeleteFruit).toHaveBeenCalledTimes(1);
+      expect(deletedLemon).toBeUndefined();
     });
   });
 
   // Task 4
   describe("storeFruitToFruitStorage", () => {
     beforeEach(async () => {
-      const lemon = new Fruit(validFruit);
       await fruitFactory.createFruit(lemon);
     });
-    /**
-     * Test 4.1 - Assume the lemon fruit is already created in task 1, in the createFruitForFruitStorage,
-     * do this in a way you see fit (e.g. seeding the database in the test) with an amount of 5 lemons.
-     * Call storeFruitToFruitStorage this should pass as it is below the limit of 10.
-     */
-    it("should store the fruit with appropriate amount", async () => {});
-    /**
-     * Test 4.2 - Assume the lemon fruit is already created in task 1, in the createFruitForFruitStorage,
-     * do this in a way you see fit (e.g. seeding the database in the test) with an amount of 11 lemons.
-     * Call storeFruitToStorage this should fail as it is above the limit of 10.
-     */
-    it("should not store the fruit if the limit is over 10", async () => {});
+    // Test 4.1
+    it("should store the fruit with appropriate amount", async () => {
+      // Arrange
+      const addAmount = FruitAmount.create({ value: 5 });
+      const mockStoreFruit = jest
+        .spyOn(fruitRepository, "storeFruitToFruitStorage")
+        .mockResolvedValueOnce();
+      // Act
+      await fruitRepository.storeFruitToFruitStorage(lemon.name, addAmount);
+      // Assert
+      expect(mockStoreFruit).toHaveBeenCalledWith(lemon.name, addAmount);
+      expect(mockStoreFruit).toHaveBeenCalledTimes(1);
+    });
+
+    // Test 4.2
+    it("should not store the fruit if the total limit is over 10", async () => {
+      // Arrange
+      const addAmount = FruitAmount.create({ value: 6 }); // 5 + 6 = 11 total
+      const mockStoreFruit = jest
+        .spyOn(fruitRepository, "storeFruitToFruitStorage")
+        .mockResolvedValueOnce();
+      // Act & Assert
+      try {
+        await fruitRepository.storeFruitToFruitStorage(lemon.name, addAmount);
+      } catch (error: any) {
+        expect(error.message).toBe(
+          new Error("Fruit amount cannot be more than 10.")
+        );
+      }
+    });
   });
 
   // Task 5
   describe("removeFruitFromFruitStorage", () => {
     beforeEach(async () => {
-      const lemon = new Fruit(validFruit);
       await fruitFactory.createFruit(lemon);
     });
-    /**
-     * Test 5.1 - Assume the lemon fruit is already created in task 1, in the createFruitForFruitStorage do
-     * this in a way you see fit (e.g. seeding the database in the test) with an amount of 5 lemons. Call
-     * removeFruitFromFruitStorage with: {name: 'lemon', amount: 5} this should pass as there are 5 lemons
-     * in storage.
-     */
-    it("should remove the fruits with appropriate amount", async () => {});
-    /**
-     * Test 5.2 - Assume the lemon fruit is already created in task 1, in the createFruitForFruitStorage, do
-     * this in a way you see fit (e.g. seeding the database in the test) with an amount of 5 lemons. Call
-     * removeFruitFromFruitStorage with: {name: 'lemon', amount: 6} this should fail as there are 5 lemons
-     * in storage.
-     */
-    it("should not remove the fruits if the limit is less than 0", async () => {});
+    // Test 5.1
+    it("should remove the fruits with appropriate amount", async () => {
+      // Arrange
+      const removeAmount = FruitAmount.create({ value: 5 });
+      const mockRemoveFruit = jest
+        .spyOn(fruitRepository, "removeFruitFromFruitStorage")
+        .mockResolvedValueOnce();
+      // Act
+      await fruitRepository.removeFruitFromFruitStorage(
+        lemon.name,
+        removeAmount
+      );
+      // Assert
+      expect(mockRemoveFruit).toHaveBeenCalledWith(lemon.name, removeAmount);
+      expect(mockRemoveFruit).toHaveBeenCalledTimes(1);
+    });
+    // Test 5.2
+    it("should not remove the fruits if the limit is less than 0", async () => {
+      // Arrange
+      const removeAmount = FruitAmount.create({ value: 6 }); // 5 - 6 = -1
+      const mockRemoveFruit = jest
+        .spyOn(fruitRepository, "removeFruitFromFruitStorage")
+        .mockResolvedValueOnce();
+      // Act & Assert
+      try {
+        await fruitRepository.removeFruitFromFruitStorage(
+          lemon.name,
+          removeAmount
+        );
+      } catch (error: any) {
+        expect(error.message).toBe(
+          new Error("Fruit amount cannot be less than 0.")
+        );
+      }
+    });
   });
 
   // Task 6
   describe("findFruit", () => {
     beforeEach(async () => {
-      const lemon = new Fruit(validFruit);
       await fruitFactory.createFruit(lemon);
     });
-    /**
-     * Test 6.1 - Assume the lemon fruit is already created in task 1, in the createFruitFromFruitStorage, do
-     * this in a way you see fit (e.g. seeding the database in the test). Call the findFruit with {name: 'lemon'}
-     * this should return the lemon object and pass.
-     */
-    it("should find out the fruit if it exists", async () => {});
-    /**
-     * Test 6.2 - Call the findFruit with {name: 'not a lemon'} this should return not the lemon object and
-     * throw an error.
-     */
-    it("should not find out the fruit if the name is wrong", async () => {});
+    // Test 6.1
+    it("should find out the fruit if it exists", async () => {
+      // Arrange
+      const lemonInstance = Fruit.create(lemon);
+      const mockFindFruit = jest
+        .spyOn(fruitRepository, "findFruit")
+        .mockResolvedValueOnce(lemonInstance);
+      // Act
+      const foundLemon = await fruitRepository.findFruit(lemon.name);
+      // Assert
+      expect(mockFindFruit).toHaveBeenCalledWith(lemon.name);
+      expect(mockFindFruit).toHaveBeenCalledTimes(1);
+      expect(foundLemon.equals(lemonInstance)).toBeTruthy();
+    });
+    // Test 6.2
+    it("should not find out fruit if the name does not exist", async () => {
+      // Arrange
+      const notExistFruitName = FruitName.create({ value: "not a lemon" });
+      // Act & Assert
+      try {
+        await fruitRepository.findFruit(notExistFruitName);
+      } catch (error: any) {
+        expect(error.message).toBe(new Error("Fruit does not exist."));
+      }
+    });
   });
 });
